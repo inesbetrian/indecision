301f78960d70fc3a2ae47b349ea7c473c7ba1801
Inés Betrián <betrianines18@gmail.com>
2025-02-27 19:04:59 +0100

    Chat con gif de YesNo

--- src/components/chat/ChatNuntii.vue
+++ src/components/chat/ChatNuntii.vue
@@ -4,7 +4,12 @@
 
         <div class="flex flex-col space-y-2">
           
-            <ChatBullula meus-est="true" nuntius="¿Salimos luego a dar una vueta?" />
+            <ChatBullula :meus-est="true" nuntius="¿Salimos luego a dar una vueta?" />
+
+            <ChatBullula 
+            :meus-est="false" 
+            nuntius="no" 
+            imago="https://yesno.wtf/assets/yes/8-2f93962e2ab24427df8589131da01a4d.gif"/>
 
         </div>
 
